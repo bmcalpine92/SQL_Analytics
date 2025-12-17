a4ec7b2c764e43e83caa9f3067fc14039a4bb43e
Brittany <bmcalpine92@gmail.com>
2025-12-17 10:20:10 -0500

    Update 01_basic_select_statements.sql
    Added additional SQL queries to practice filtering mechanics.

--- queries/01_basic_select_statements.sql
+++ queries/01_basic_select_statements.sql
@@ -2,18 +2,20 @@
 -- Dataset: Dunder Mifflin Paper Sales
 -- Purpose: Demonstrate basic table exploration, filtering, and business insights
 
+
+---------------------------------------------------
+-- SECTION 1: Dataset Exploration (Q1–Q3)
 ---------------------------------------------------
 
+
 -- Question #1: What data does the table contain, and what fields are available?
--- Preview the dataset structure and sample records
-SELECT *
+SELECT *        -- Preview the dataset structure and sample records
 FROM tutorial.dunder_mifflin_paper_sales
 LIMIT 10;
 
 ---------------------------------------------------
 
 -- Question #2: How many total sales are in the dataset?
--- Count all rows to understand dataset size
 SELECT
     COUNT(*) AS total_sales
 FROM tutorial.dunder_mifflin_paper_sales;
@@ -21,12 +23,15 @@ FROM tutorial.dunder_mifflin_paper_sales;
 ---------------------------------------------------
 
 -- Question #3: Which account managers are represented in the dataset?
--- Retrieve a distinct list of account managers
-SELECT DISTINCT account_manager
+SELECT DISTINCT account_manager       -- Retrieve a distinct list of account managers
 FROM tutorial.dunder_mifflin_paper_sales
 ORDER BY account_manager; 
 
+
 ---------------------------------------------------
+-- SECTION 2: Basic Filtering (Q4–Q6)
+---------------------------------------------------
+
 
 -- Question #4: Show the 10 most recent sales from the state of Pennsylvania.
 SELECT 
@@ -56,7 +61,6 @@ ORDER BY price DESC;
 ---------------------------------------------------
 
 -- Question #6: Retrieve all sales records where the status is not 'COMPLETED'.
--- Includes all sales that are cancelled, returned, or have a NULL status
 SELECT 
   account_name,
   account_manager,
@@ -64,4 +68,129 @@ SELECT
   price as total_sale_amount,
   status
 FROM tutorial.dunder_mifflin_paper_sales
-WHERE status <> 'COMPLETED';  
+WHERE status <> 'COMPLETED';         -- Includes all sales that are cancelled or returned
+
+
+---------------------------------------------------
+-- SECTION 3: Intermediate Filtering (Q7–Q12)
+---------------------------------------------------
+
+
+-- Question #7: Retrieve all orders that have a status of 'Completed' AND a price greater than 100.
+SELECT
+  account_name,
+  account_manager,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  status
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE status = 'COMPLETED' AND price > 100
+ORDER BY purchased_at DESC, account_manager; 
+
+---------------------------------------------------
+
+-- Question #8: Find all orders that were cancelled OR returned.
+SELECT
+  account_name,
+  account_manager,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  status
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE status = 'CANCELLED' OR status = 'RETURNED'
+ORDER BY purchased_at DESC, account_manager; 
+
+---------------------------------------------------
+
+-- Question #9: Orders where days_to_close is between 5 and 15 days.
+SELECT
+  account_name,
+  account_manager,
+  purchased_at as sale_date,
+  days_to_close
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE days_to_close BETWEEN 5 AND 15
+ORDER BY days_to_close DESC, account_manager, purchased_at; 
+
+---------------------------------------------------
+
+-- Question #10: Find all the accounts where account_name contains 'LLC'  
+SELECT account_name
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE account_name LIKE '% LLC'
+ORDER BY account_name;
+
+---------------------------------------------------
+
+-- Question #11: Retrieve all shipping cities that do not start with 'New'.
+SELECT 
+  shipping_city AS city, 
+  shipping_state AS state
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE shipping_city NOT LIKE 'New %'
+ORDER BY shipping_city;
+
+---------------------------------------------------
+
+-- Question #12: Find all orders shipped to Pennsylvania, Ohio, and New York.
+SELECT
+  account_name,
+  account_manager,
+  shipping_state as state,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  status
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE shipping_state IN ('PA', 'OH', 'NY')
+ORDER BY shipping_state;
+
+
+---------------------------------------------------
+-- SECTION 4: Business Scenarios (Q13–Q15)
+---------------------------------------------------
+
+
+-- Question #13: List all orders that have been reviewed.  
+SELECT
+  account_name,
+  account_manager,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  status,
+  rating
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE reviewed_at IS NOT NULL
+ORDER BY rating, account_manager;
+
+---------------------------------------------------
+
+-- Question #14: Retreive all orders that: have a price BETWEEN 75 AND 300, were shipped to California, Oregon, and Utah, and have been reviewed.
+SELECT
+  account_name,
+  account_manager,
+  shipping_state as state,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  rating
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE price BETWEEN 75 AND 300
+  AND shipping_state IN ('CA', 'OR', 'UT')
+  AND rating IS NOT NULL
+ORDER BY shipping_state, rating, account_manager;
+
+---------------------------------------------------
+
+-- Question #15: Identify high-value orders where: the quantity is greater than 50, a discount is applied, and the shipping method is Express'
+SELECT
+  account_name,
+  account_manager,
+  purchased_at as sale_date,
+  price as total_sale_amount,
+  discount,
+  quantity,
+  shipping_mode
+FROM tutorial.dunder_mifflin_paper_sales
+WHERE quantity > 50
+  AND discount IS NOT NULL
+  AND shipping_mode = 'EXPRESS'
+ORDER BY account_manager;
